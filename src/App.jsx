653680e9b5cc9bd6e22591450fa1f2ca4fa92fad
mehedihasansufi <hasanmehedi205324@gmail.com>
2026-03-29 18:38:20 +0600

    Added Mens Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
+import { Suspense } from "react"
 import Hero from "./components/hero/Hero"
+import Mens from "./components/mens/Mens"
 import Navbar from "./components/navbar/Navbar"
 
 
-
+const MensApi = fetch("mens.json").then(res => res.json())
 
 
 function App() {
- 
+
 
   return (
-   <>
-   <Navbar></Navbar>
-   <Hero></Hero>
-  <popular></popular>
-   </>
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Mens MensApi={MensApi}></Mens>
+      </Suspense>
+    </>
   )
 }
 
---
+++ src/components/productCard/ProductCard.jsx
@@ -0,0 +1,40 @@
+import { Heart } from 'lucide-react';
+
+
+const ProductCard = ({data}) => {
+    console.log(data.image);
+    
+    
+    return (
+        <div >
+
+            <div className=''>
+                <div className='relative'>
+                    <img src={data.image} alt="" />
+                    <p className='bg-white absolute bottom-2 uppercase left-4 px-3 text-xs  font-bold rounded-xs py-1'>Out Of Stock</p>
+                    <div className='absolute top-2 bg-white p-2 w-8 h-8 flex justify-center items-center right-2 rounded-full'>
+                        <Heart strokeWidth={1} width={15}></Heart>
+                    </div>
+
+                </div>
+
+                <div className='px-2 mt-1'>
+                    <div className='flex justify-between'>
+                        <p>{data.name}</p>
+                        <p className='font-bold text-xs'>{data.rating}</p>
+                    </div>
+
+
+                    <div className='flex flex-col gap-1 mt-1'>
+                        <p className='text-[#00000090]'>{data.fit}</p>
+                        <p className='text-[#00000090]'>{data.color}</p>
+                        <p className='font-bold'>US${data.price}</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default ProductCard;
